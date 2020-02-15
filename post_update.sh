774923673f822815aa13b63a955ab09d0517cdc5
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2020-02-15 10:13:04 -0500

    disable esphome menu

--- post_update.sh
+++ post_update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-install.extra.pkgs() {
+install_extra_pkgs() {
   ## Install any remaining packages from '/tmp/pkglist'
   ## This is used to reinstall any additional pakages not included in the plugin manifest
   pkgs=$(cat /tmp/pkglist)
@@ -8,14 +8,15 @@ install.extra.pkgs() {
   echo "${pkgs}" | xargs pkg install -y
 }
 
-update.post_install() {
+update_post_install() {
   ## `post_install.sh` doesn't seem to get updated on it's own. Likely it's only files in the overlay
-  ## I should probably move stuff out of the post_install script to avoid this in the future.
-  wget -O /root/post_install.sh https://raw.githubusercontent.com/tprelog/iocage-homeassistant/11.3-RELEASE/post_install.sh \
+  ## I should probably move stuff out of the post_install script to avoid this need in the future.
+  echo -e "\nUpdating post_install.sh..."
+  wget -q -O /root/post_install.sh https://raw.githubusercontent.com/tprelog/iocage-homeassistant/11.3-RELEASE/post_install.sh \
   && chmod +x /root/post_install.sh
 }
 
-rename.hasshelper() {
+rename_hass_helper() {
 ## hass-helper is now hassbsd -- This should handle that change.
 if [ -f /root/bin/hass-helper ] && [ -f /root/bin/hassbsd ]; then
   sed -e "s/hass-helper/hassbsd/g" /root/.login > /root/.loginTemp \
@@ -24,7 +25,20 @@ if [ -f /root/bin/hass-helper ] && [ -f /root/bin/hassbsd ]; then
 fi
 }
 
-rename.hasshelper
-install.extra.pkgs
-update.post_install
+check_esphome () {
+## ESPHome has been moved to a seperate FreeNAS plugin. This should re-enable the
+## console menu for people who had it already installed before it was removed.
+  echo -e "\nChecking for ESPHome..."
+  sysrc esphome_enable  2> /dev/null
+  if [ $? == 0 ]; then
+    sysrc esphome_menu=1
+  else
+    sysrc -x esphome_menu 2> /dev/null
+  fi
+}
+
+install_extra_pkgs
+rename_hass_helper
+update_post_install
+check_esphome
 echo -e "\npost_update.sh Finished\n"
