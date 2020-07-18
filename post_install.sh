9511b199aae1c5b0e146ba06397fe80751567464
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2020-07-18 17:45:25 -0400

    :recycle: post_install - all for one and one for all

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+plugin_ver="v_0.4.0"
 srv_name="homeassistant"
 
 . /etc/rc.subr
 load_rc_config
-plugin_ver="${plugin_ver:="v_0.4.0"}"
 
 #
 # plugin_srv_prefix: Directory where virtualenv directories are located.
@@ -11,37 +11,22 @@ plugin_ver="${plugin_ver:="v_0.4.0"}"
 #       Set to retro:    `sysrc plugin_srv_prefix=/srv`
 #       Reset to default: `sysrc -x plugin_srv_prefix`
 
-# ( Hopefully ) sane default for *BSD / FreeNAS / TrueNAS Core
-srv_prefix="${plugin_srv_prefix:="/usr/local/share"}"
-
-srv_enable="${homeassistant_enable:-"yes"}"
-srv_umask="${homeassistant_umask:-"002"}"
-# UUID matches the default port used by homeassistant
-srv_uuid="${homeassistant_uid:-"8123"}"
-# USER / GROUP to run the homeassistant daemon
-srv_uname="${homeassistant_user:-"homeassistant"}"
-srv_gname="${homeassistant_group:-"homeassistant"}"
-# user_dir is HOME and PARENT_DIR to SERVICE configuration
-srv_uhome="${homeassistant_user_dir:-"/home/homeassistant"}"
-# locatation for homeassistant confiuragtion files
-srv_config_dir="${homeassistant_config_dir:-"/home/homeassistant/homeassistant"}"
-# path to python version to use for the virtualenv
-srv_python="${homeassistant_python:-"/usr/local/bin/python3.8"}"
-# full path to the python virtualenv for homeassistant
-srv_venv="${homeassistant_venv:-"${srv_prefix}/${srv_name}"}"
-
-
-set_rc_vars() {
-  echo -e "\nsetting rc vars for \"${srv_name}\""
-  sysrc ${srv_name}_enable="${srv_enable}"
-  sysrc ${srv_name}_config_dir="${srv_config_dir}"
-  sysrc ${srv_name}_user="${srv_uname}"
-  sysrc ${srv_name}_group="${srv_gname}"
-  sysrc ${srv_name}_python="${srv_python}"
-  sysrc ${srv_name}_venv="${srv_venv}"
-  sysrc ${srv_name}_umask="${srv_umask}"
-  echo
-}
+# ( Hopefully ) sane defaults for *BSD jails / FreeNAS / TrueNAS Core
+srv_prefix="${plugin_srv_prefix:-"/usr/local/share"}"
+srv_uuid="${plugin_srv_uuid:-"8123"}"
+srv_umask="${plugin_srv_umask:-"002"}"
+srv_uname="${plugin_srv_uname:-"${srv_name}"}"
+srv_gname="${plugin_srv_gname:-"${srv_name}"}"
+srv_uhome="${plugin_srv_uhome:-"/home/${srv_name}"}"
+srv_config_dir="${plugin_srv_config_dir:-"${srv_uhome}/${srv_name}"}"
+srv_venv="${plugin_srv_venv:-"${srv_prefix}/${srv_name}"}"
+## Set python version -- try python 3.8 first
+[ ! -z "${_python:="$(which "${plugin_srv_python}")"}" ] \
+|| [ ! -z "${_python:="$(which python3.8)"}" ] \
+|| [ ! -z "${_python:="$(which python3.7)"}" ] \
+|| err 1 "please set python using 'sysrc plugin_srv_python=/path/to/python'"
+debug "using python: ${_python}"
+srv_python="${_python}"
 
 
 add_user() {
@@ -49,11 +34,29 @@ add_user() {
   local _D_="${srv_uhome}"
   local _N_="${srv_uname}"
   local _G_="${srv_gname}"
-  info "adding user \"${_N_}\""
+  info "adding user ${_N_}"
   pw adduser -u "${_U_}" -n "${_N_}" -d "${_D_}" -w no -s /usr/local/bin/bash -G dialer 2>/tmp/err \
   || return 1
   install -d -m 775 -o "${_N_}" -g "${_G_}" -- "${_D_}"
   chown -R "${_N_}":"${_G_}" "${_D_}"
+}
+
+
+set_rc_vars() {
+  echo -e "\nINFO: setting rc vars for ${srv_name}"
+  #sysrc ${srv_name}_enable="${srv_enable}"
+  if [ -z "${_dir_:-"$(sysrc -n ${srv_venv} 2>/dev/null)"}" ]; then
+    if [ -d "/srv/${srv_name}" ]; then
+      echo "RETRO VENV: found directory. setting maunal override to use existing directory"
+      srv_venv="/srv/${srv_name}"
+    fi
+  fi
+  sysrc ${srv_name}_venv="${srv_venv}"
+  sysrc ${srv_name}_python="${srv_python}"
+  sysrc ${srv_name}_umask="${srv_umask}"
+  sysrc ${srv_name}_user="${srv_uname}"
+  sysrc ${srv_name}_group="${srv_gname}"
+  sysrc ${srv_name}_config_dir="${srv_config_dir}"
 }
 
 
@@ -64,12 +67,13 @@ _config_warning() {
   sleep 1
 }
 
+
 cp_config() {
   ## ONLY IF ${config_dir} == EMPTY, else nothing is copied.
   ## copy the example configuration files during an install.
   ## These files should be modified or replaced by end users
   srv_name="${1:-"${srv_name}"}"
-  config_dir="${2:-"${srv_config_dir}"}"
+  config_dir="${srv_config_dir}"
   example_cfg="/usr/local/examples/${srv_name}/"
   debug "copy example config for ${srv_name}: ${config_dir}"
   
@@ -150,7 +154,6 @@ install_service() {
     install -d -g ${srv_gname} -m 775 -o ${srv_uname} -- ${srv_venv} 2>/tmp/err \
     || err ${?} "$(</tmp/err)"
   elif [ ! -z "$(ls -A ${srv_venv})" ] && [ "${__name__}" = "post_install.sh" ]; then
-  # TODO allow this function to pass if ${1} == ${srv_name}
     warn "${orn}virtualenv directory found and it's not empty!${end}"
     warn "${orn}please remove \"${srv_venv}\" and try again${end}"
     err 1 "${red}expecting empty directory${end} ${srv_venv}"
@@ -207,33 +210,42 @@ v2srv_ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9
 #case "${__name__}" in
 if [ "${__name__}" == "post_install.sh" ] && [ -z "${1}" ]; then
 #  "post_install.sh")
-    ## WARNING Running this script, `post_install.sh` ( with NO_ARGS ), is intended for the initial installation only
-    ## Install the Home Assistant Core -- Community  Plugin (iocage) -- Tested on FreeNAS 11.3 / TrueNAS Core 12.x
+    ## WARNING Running this script, 'post_install.sh' with NO_ARGS, is intended for the initial installation only
+    ## Install the Home Assistant Core (iocage) Community Plugin - Tested on FreeNAS 11.3 / TrueNAS Core 12.x
 #    rc_debug="ON"
     add_user || err 1 "$(</tmp/err)"
     sysrc plugin_ver="${plugin_ver}"
     sysrc plugin_ini="${plugin_ver}_$(date +%Y%m%d)"
     set_rc_vars
     cp_config ${srv_name}
-    service ${srv_name} install ${srv_name} || err 1 "return $?"
-    service ${srv_name} config --check
+    service ${srv_name} oneinstall ${srv_name} || err 1 "return $?"
+#    service ${srv_name} config --check
     enableStart_v2srv
-    ## WARNING Running the initial installation multiple times, will result in multiple entries to launch the menu
     ## Start the console menu upon login
+    ## WARNING Running the initial installation multiple times, will result in multiple entries to launch the menu
     echo -e "\n# Start console menu after login." >> /root/.login
     echo "if ( -x /root/bin/menu ) menu" >> /root/.login
     ## TODO Add someting useful to this
-    echo "useful information coming soon!" > /root/PLUGIN_INFO
+    echo "version: ${plugin_ver}" > /root/PLUGIN_INFO
+    echo -e "\nInitial startup can take 5-10 minutes before Home Assistant is reachable." >> /root/PLUGIN_INFO
+    echo -e " useful information coming soon!\n" >> /root/PLUGIN_INFO
     exit "${?}"
 #  ;;
 elif [ "${__name__}" == "homeassistant" ] || [ "${1}" == "homeassistant" ]; then
 #  "homeassistant")
     srv_name="homeassistant"
     info "service \"${__name__}\" has called post_install.sh ${srv_name}"
-    install_service ${srv_name} || exit
+    srv_umask="${homeassistant_umask:-"${srv_umask}"}"
+    srv_uname="${homeassistant_user:-"$(id -unr "${srv_uuid}")"}"
+    srv_gname="${homeassistant_group:-"$(id -nrg "${srv_uname}")"}"
+    srv_uhome="${homeassistant_user_dir:-"$(eval echo "~${srv_uname}" 2>/dev/null)"}"
+    srv_config_dir="${homeassistant_config_dir:-"${srv_uhome}/${srv_name}"}"
+    srv_python="${homeassistant_python:-"${srv_python}"}"
+    srv_venv="${homeassistant_venv:-"${srv_prefix}/${srv_name}"}"
+    install_service ${srv_name} || err 1 "return $?"
     set_rc_vars
     cp_config ${srv_name}
-    enableStart_v2srv || exit
+    enableStart_v2srv || err 1 "return $?"
     echo -e "\n ${grn}http://${v2srv_ip}:8123${end}\n"
     echo -e "${orn}Initial startup can take several minutes before Home Assistant is fully loaded${end}\n"
     exit "${?}"
@@ -242,25 +254,23 @@ elif [ "${__name__}" == "appdaemon" ] || [ "${1}" == "appdaemon" ]; then
 #  "appdaemon")
     srv_name="appdaemon"
     info "service \"${__name__}\" has called post_install.sh ${srv_name}"
-#     srv_prefix="${plugin_srv_prefix:-"/usr/local/share"}"
-    srv_enable="${appdaemon_enable:-"yes"}"
-    srv_umask="${appdaemon_umask:-"002"}"
-    # USER / GROUP to run the appdaemon daemon
-    srv_uname="${appdaemon_user:-"homeassistant"}"
-    srv_gname="${appdaemon_group:-"homeassistant"}"
-    # user_dir is HOME and PARENT_DIR to SERVICE configuration
-    srv_uhome="${appdaemon_user_dir:-"/home/homeassistant"}"
-    # locatation for appdaemon confiuragtion files
-    srv_config_dir="${appdaemon_config_dir:-"/home/homeassistant/appdaemon"}"
-    # path to python version to use for the virtualenv
-    srv_python="${appdaemon_python:-"/usr/local/bin/python3.8"}"
-    # full path to the python virtualenv for appdaemon
+    srv_umask="${appdaemon_umask:-"${srv_umask}"}"
+    srv_uname="${appdaemon_user:-"$(id -unr "${srv_uuid}")"}"
+    srv_gname="${appdaemon_group:-"$(id -nrg "${srv_uname}")"}"
+    srv_uhome="${appdaemon_user_dir:-"$(eval echo "~${srv_uname}" 2>/dev/null)"}"
+    srv_config_dir="${appdaemon_config_dir:-"${srv_uhome}/${srv_name}"}"
+    srv_python="${appdaemon_python:-"${srv_python}"}"
     srv_venv="${appdaemon_venv:-"${srv_prefix}/${srv_name}"}"
-    # TODO TN > service ${srv_name} install ${srv_name} || err 1 "return $?"
-    install_service ${srv_name} || exit
+    install_service ${srv_name} || exit "${?}"
     set_rc_vars
     cp_config ${srv_name}
-    enableStart_v2srv || exit
+    debug "checking for nested config directory - ${srv_config_dir}/conf"
+    if [ -d "${_config:="${srv_config_dir}/conf"}" ]; then
+      ## If the directory is not already there - it should no longer be provided by this plugin
+      echo "RETRO CONFIG: directory found - setting manual override to use existing configuration"
+      sysrc appdaemon_config_dir="${_config}"
+    fi
+    enableStart_v2srv || exit "${?}"
     echo -e "\n ${grn}http://${v2srv_ip}:5050${end}\n"
     echo -e "You may need to restart Home Assistant for all changes to take effect\n"
     exit "${?}"
@@ -269,26 +279,23 @@ elif [ "${__name__}" == "configurator" ] || [ "${1}" == "configurator" ]; then
 #  "configurator")
     srv_name="configurator"
     info "service \"${__name__}\" has called post_install.sh ${srv_name}"
-#     srv_prefix="${plugin_srv_prefix:-"/usr/local/share"}"
-    srv_enable="${configurator_enable:-"yes"}"
-    srv_umask="${configurator_umask:-"002"}"
-    # USER / GROUP to run the configurator daemon
-    srv_uname="${configurator_user:-"homeassistant"}"
-    srv_gname="${configurator_group:-"homeassistant"}"
-    # user_dir is HOME and PARENT_DIR to SERVICE configuration
-    srv_uhome="${configurator_user_dir:-"/home/homeassistant"}"
-    # locatation for configurator confiuragtion files
-    srv_config_dir="${configurator_config_dir:-"/home/homeassistant/configurator"}"
-    # path to python version to use for the virtualenv
-    srv_python="${configurator_python:-"/usr/local/bin/python3.8"}"
-    # full path to the python virtualenv for configurator
+    srv_umask="${configurator_umask:-"${srv_umask}"}"
+    srv_uname="${configurator_user:-"$(id -unr "${srv_uuid}")"}"
+    srv_gname="${configurator_group:-"$(id -nrg "${srv_uname}")"}"
+    srv_uhome="${configurator_user_dir:-"$(eval echo "~${srv_uname}" 2>/dev/null)"}"
+    srv_config_dir="${configurator_config_dir:-"${srv_uhome}/${srv_name}"}"
+    srv_python="${configurator_python:-"${srv_python}"}"
     srv_venv="${configurator_venv:-"${srv_prefix}/${srv_name}"}"
-    # TODO TN > service ${srv_name} install ${srv_name} || err 1 "return $?"
-    install_service ${srv_name} || exit
+    install_service ${srv_name} || err 1 "return $?"
     set_rc_vars
     cp_config ${srv_name}
-    enableStart_v2srv \
-    || exit
+    debug "checking for retro config file - ${srv_config_dir}/configurator.conf"
+    if [ -f "${_config:="${srv_config_dir}/configurator.conf"}" ]; then
+      ## If the file is not already there - it should no longer be provided by this plugin
+      echo "RETRO CONFIG: file found - setting manual override to use existing configuration"
+      sysrc configurator_config="${_config}"
+    fi
+    enableStart_v2srv || exit "${?}"
     echo -e "\n ${grn}http://${v2srv_ip}:3218${end}\n"
     echo -e "You may need to restart Home Assistant for all changes to take effect\n"
     exit "${?}"
