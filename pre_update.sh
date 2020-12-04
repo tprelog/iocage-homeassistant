568e40e4110de502a323b1f780b157cd32866f55
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2020-12-04 10:35:57 -0500

    update comments :pencil2:

--- pre_update.sh
+++ pre_update.sh
@@ -13,14 +13,14 @@ update_post_install() {
 # shellcheck disable=SC2154
 if [ "${plugin_ver}" == "v_0.4.0" ]; then
   warn "Version 5 is now available! Please see the wiki for breaking changes."
-  warn "You will need a fresh install of this plugin in order to upgrade!"
+  warn "You may need a fresh install of this plugin in order to upgrade!"
   update_post_install
 elif [ "${plugin_version%%.*}" == "5" ]; then
   true
 else ## if plugin_ver != 4 then suggested a fresh install and fail.
 # TODO if plugin_force_update then attempt to force upgrade (useful for debugging)
   warn "Version 5 now is available! Please see the wiki for breaking changes."
-  warn "Unsupported update path! I suggest to you, please reinstall this plugin."
+  warn "Unsupported update path! Please reinstall this plugin."
   err 1 "BREAKING CHANGES - Manual intervention is required!"
 fi
 
