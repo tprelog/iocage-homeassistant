0707fe5b4b905b57a35a2fb1445a835cb508309d
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2021-02-24 22:27:40 -0500

    :recycle: refactor update scripts

--- post_update.sh
+++ post_update.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
-# plugin version 5.0
 
+# shellcheck disable=SC1091
 . /etc/rc.subr && load_rc_config
-: "${plugin_enable_primelist:="NO"}"
+: "${plugin_enable_pkglist:="NO"}"
+: "${plugin_upgrade_service:="NO"}"
 
-install_primelist() {
-  ## If enabled, re-install packages from the prime-list
-  ## Use `sysrc plugin_enable_primelist=YES` to enable
-  local pkgs ; pkgs=$(cat "${plugin_primelist:-/tmp/pkglist}")
-  echo -e "\nChecking prime-list for additional packages..."
+install_pkglist() {
+  ## If enabled, re-install packages from a pkglist, after a Plugin UPDATE
+  ## Use `sysrc plugin_pkglist="/path/to/pkglist"` to set a pkglist
+  ## Use `sysrc plugin_enable_pkglist=YES` to enable
+  local pkgs ; pkgs=$(cat "${plugin_pkglist:-/dev/null}")
+  echo -e "\nChecking for additional packages to install..."
   echo "${pkgs}" | xargs pkg install -y
 }
 
-checkyesno plugin_enable_primelist && install_primelist
+upgrade_service() {
+  ## If enabled, upgrade Home Assistant Core during a Plugin UPDATE
+  ## Use `sysrc plugin_upgrade_service=YES` to enable
+  service homeassistant install --upgrade homeassistant
+}
 
+checkyesno plugin_enable_pkglist && install_pkglist
+checkyesno plugin_upgrade_service && upgrade_service
+
+sysrc plugin_version="$(cat /root/.PLUGIN_VERSION)"
+
+## Gererate PLUGIN_INFO
 echo "TODO: Update the PLUGIN_INFO"
+#/root/.plugin/bin/plugin-info
--- pre_update.sh
+++ pre_update.sh
@@ -1,32 +1,19 @@
 #!/usr/bin/env bash
-# plugin version 5.0
 
+# shellcheck disable=SC1091
 . /etc/rc.subr && load_rc_config
-
-update_post_install() {
-  ## post_install.sh is not automatically updated after the initial installation. (This should NOT be required)
-  ## FIXED - Version 5 does not require any updates to post_install.sh after initial installation. Still, we
-  ## should update the script in version 4, but disable exe to midigate potential use - It should NOT be used!
-  wget -q -O /root/post_install.sh https://raw.githubusercontent.com/tprelog/iocage-homeassistant/master/post_install.sh \
-  && chmod -x /root/post_install.sh
-}
 
 # shellcheck disable=SC2154
 if [ "${plugin_ver}" == "v_0.4.0" ]; then
-  warn "Version 5 is now available! Please see the wiki for breaking changes."
+  warn "Version 6 is now available! Please see the wiki for breaking changes."
   warn "You may need a fresh install of this plugin in order to upgrade!"
-  update_post_install
+  rm -f /root/post_install.sh
 elif [ "${plugin_version%%.*}" == "5" ]; then
-  true
+  ## TODO -- for now just exit 0
+  exit 0
 elif [ "${plugin_version%%-*}" == "6" ]; then
-  true
-else ## if plugin_ver != 4 then suggested a fresh install and fail.
-# TODO if plugin_force_update then attempt to force upgrade (useful for debugging)
-  warn "Version 5 now is available! Please see the wiki for breaking changes."
-  warn "Unsupported update path! Please reinstall this plugin."
-  err 1 "BREAKING CHANGES - Manual intervention is required!"
+  exit 0
+else
+  warn "Upgrades from unknown versions are no longer supported! Please reinstall this plugin."
+  err 1 "Manual intervention is required! Please see the wiki for breaking changes."
 fi
-
-## Generate a list of the primaray packages that have been installed.
-## If enabled, these packages will be re-installed after Plugin -> UPDATE.
-pkg prime-list > /tmp/pkglist
