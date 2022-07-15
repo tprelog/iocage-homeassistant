34ac22e0634131d20ed5f93d4b69b3e014525a8e
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2022-07-14 20:24:31 -0400

    fix bad substitution

--- pre_update.sh
+++ pre_update.sh
@@ -3,9 +3,9 @@
 . /etc/rc.subr && load_rc_config
 
 # shellcheck disable=SC2154
-ver="${plugin_version%%-*}"
+version=$(echo "${plugin_version%%-*}" | cut -c2-)
 
-if [[ "${ver:1:2}" -lt 6 ]]; then
+if ! [ "${version:-0}" -ge 6 ]; then
   warn "  Unknown or unsupported version detected. Manual intervention required! "
   err 1 " You must reinstall the plugin to perform this upgrade. "
 fi
